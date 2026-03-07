2056ae5dad9555e5ea1ba336ccb7e6496b4239d1
ratulhasanruhan <ratulhasan1644@gmail.com>
2026-03-07 06:28:31 +0600

    tested

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:care_life_touch/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('App boots and main navigation tabs work', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.text('Products'), findsOneWidget);
+    expect(find.text('My Bag'), findsOneWidget);
+    expect(find.text('More'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('Products'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Products'), findsWidgets);
+    expect(find.text('Search Your Needs...'), findsOneWidget);
   });
 }
